f896ee790159e2c4858da95eb502bd670ec9c89c
Cpt0Teemo <ypoulmarck@gmail.com>
2021-04-22 13:12:45 -0400

    Added function to generate XMI for xtext

---
+++ org.xtext.example.warehouse/src/org/xtext/example/warehouse/Main.java
@@ -0,0 +1,44 @@
+package org.xtext.example.warehouse;
+
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.resource.XtextResourceSet;
+
+import com.google.inject.Injector;
+
+public class Main {
+
+	public static void main(String[] args) {
+		exportXMI("/Users/yoanpolumarck/Documents/repo/runtime-EclipseXtext/tt");
+
+	}
+	
+	private static void exportXMI(String absuloteTargetFolderPath) {
+	    // change MyLanguage with your language name
+	    Injector injector = new WarehouseModellingStandaloneSetup()
+	            .createInjectorAndDoEMFRegistration();
+	    XtextResourceSet resourceSet = injector
+	            .getInstance(XtextResourceSet.class);
+
+	    // .ext ist the extension of the model file
+	    String inputURI = "file:///" + absuloteTargetFolderPath + "/testing.warehouse";
+	    String outputURI = "file:///" + absuloteTargetFolderPath + "/output.xmi";
+	    URI uri = URI.createURI(inputURI);	
+	    Resource xtextResource = resourceSet.getResource(uri, true);
+
+	    EcoreUtil.resolveAll(xtextResource);
+
+	    Resource xmiResource = resourceSet
+	            .createResource(URI.createURI(outputURI));
+	    xmiResource.getContents().add(xtextResource.getContents().get(0));
+	    try {
+	        xmiResource.save(null);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+}
